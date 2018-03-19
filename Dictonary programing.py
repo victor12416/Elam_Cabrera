a1f2658ba8503c8600f1c896db401529c3dd7225
Elam Delgado cabrera <elam.delgadocabrera@stu.jefferson.kyschools.us>
2018-03-19 09:18:01 -0400

    Third commit

---
+++ Dictonary programing.py
@@ -0,0 +1,12 @@
+#3/17/2018
+
+Elam_vocab = {
+    "Minecarft": "how to be a nerd",
+    "Rowblocks":  "how to be a bad nerd",
+    "Terreria": "how to wate your time ",
+    "Fortnite": "the one game that i dont know how to play",
+    "Hallelujah": "when you get them dimonds in minecarft",
+    "Motherload": "is when you find that one dimond and you have forntune 3 on you iron pick."
+    }
+for k,v in Elam_vocab.items():
+    print (k,v)
